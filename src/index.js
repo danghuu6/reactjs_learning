46f90faeb260b89a18ed1cd0585223e031b33d53
Dang Nguyen <nguyenhuudang0206@gmail.com>
2023-06-05 21:52:08 +0700

    Update version react 17 to 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 function App() {
     
@@ -10,4 +10,5 @@ function App() {
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
